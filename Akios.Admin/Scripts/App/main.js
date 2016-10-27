28613266a1e4d326950ec61c5403ed2f055e235d
ibrhmoguz <ibrahimoguz.ie@gmail.com>
2016-10-27 17:25:17 +0300

    19

--- Akios.Admin/Scripts/App/main.js
+++ Akios.Admin/Scripts/App/main.js
@@ -166,104 +166,21 @@ $.extend(true, $.fn.dataTable.defaults, {
 });
 
 
-/********** JQUERY FILE UPLOAD  **********/
-
-function FileUpload(url, id) {
-    var uploadButton = $('<button/>')
-        .addClass('btn btn-primary')
-        .prop('disabled', true)
-        .text('Processing...')
-        .on('click', function () {
-            var $this = $(this),
-                data = $this.data();
-            $this
-                .off('click')
-                .text('Abort')
-                .on('click', function () {
-                    $this.remove();
-                    data.abort();
-                });
-            data.submit().always(function () {
-                $this.remove();
-            });
-        });
-    $('#' + id).fileupload({
-        url: url,
-        dataType: 'json',
-        autoUpload: true,
-        singleFileUploads: true,
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-        maxFileSize: 999000,
-        // Enable image resizing, except for Android and Opera,
-        // which actually support image resizing, but fail to
-        // send Blob objects via XHR requests:
-        disableImageResize: /Android(?!.*Chrome)|Opera/
-            .test(window.navigator.userAgent),
-        previewMaxWidth: 100,
-        previewMaxHeight: 100,
-        previewCrop: false
-    }).on('fileuploadadd', function (e, data) {
-        data.context = $('<div/>').appendTo('#files');
-        $.each(data.files, function (index, file) {
-            var node = $('<p/>')
-                .append($('<span/>').text(file.name));
-            if (!index) {
-                node
-                    .append('<br>')
-                    .append(uploadButton.clone(true).data(data));
-            }
-            node.appendTo(data.context);
-        });
-    }).on('fileuploadprocessalways', function (e, data) {
-        var index = data.index,
-            file = data.files[index],
-            node = $(data.context.children()[index]);
-        if (file.preview) {
-            node
-                .prepend('<br>')
-                .prepend(file.preview);
-        }
-        if (file.error) {
-            node
-                .append('<br>')
-                .append($('<span class="text-danger"/>').text(file.error));
-        }
-        if (index + 1 === data.files.length) {
-            data.context.find('button')
-                .text('Upload')
-                .prop('disabled', !!data.files.error);
-        }
-    }).on('fileuploadprogressall', function (e, data) {
-        var progress = parseInt(data.loaded / data.total * 100, 10);
-        $('#progress .progress-bar').css(
-            'width',
-            progress + '%'
-        );
-    }).on('fileuploaddone', function (e, data) {
-        $.each(data.result.files, function (index, file) {
-            if (file.url) {
-                var link = $('<a>')
-                    .attr('target', '_blank')
-                    .prop('href', file.url);
-                $(data.context.children()[index])
-                    .wrap(link);
-            } else if (file.error) {
-                var error = $('<span class="text-danger"/>').text(file.error);
-                $(data.context.children()[index])
-                    .append('<br>')
-                    .append(error);
-            }
-        });
-    }).on('fileuploadfail', function (e, data) {
-        $.each(data.files, function (index) {
-            var error = $('<span class="text-danger"/>').text('File upload failed.');
-            $(data.context.children()[index])
-                .append('<br>')
-                .append(error);
-        });
-    }).prop('disabled', !$.support.fileInput)
-        .parent().addClass($.support.fileInput ? undefined : 'disabled');
-}
+/********** DROPZONE JS  **********/
+Dropzone.options.PictureUploadForm = {
+    maxFilesize: 2, // MB
+    addRemoveLinks: true,
+    maxFiles: 1,
+    dictDefaultMessage: "Dosya yüklemek için buraya sürükleyiniz.",
+    dictFallbackMessage: "Tarayıcınız sürükle-bırak dosya yüklemeyi desteklemiyor.",
+    dictInvalidFileType: "bu dosya türünü yükleyemezsiniz.",
+    dictFileTooBig: "Yüklediğiniz dosya {{filesize}}, dosya boyutu {{maxFilesize}} geçemez!",
+    dictResponseError: "Dosya yüklenemedi!.",
+    dictCancelUpload: "İptal",
+    dictCancelUploadConfirmation: "İptali Onayla",
+    dictRemoveFile: "Sil",
+    dictMaxFilesExceeded: "Birden fazla dosya yükleyemezsiniz."
+};
 
 
 /********** COMMON FUNCTIONS *************/
